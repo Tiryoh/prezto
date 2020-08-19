b40dab0c64d890f1001026be956d111e432a98f1
Tiryoh <tiryoh@gmail.com>
2020-08-19 23:43:23 +0900

    feat: Add nodebrew completion

--- modules/completion/opt/update.sh
+++ modules/completion/opt/update.sh
@@ -8,3 +8,6 @@ if [ "$1" = "git" ]; then
 	curl -sL https://github.com/git/git/raw/master/contrib/completion/git-completion.zsh > _git
 	curl -sL https://github.com/git/git/raw/master/contrib/completion/git-completion.bash > git-completion.bash
 fi
+if [ "$1" = "nodebrew" ]; then
+	curl -sL https://github.com/hokaccha/nodebrew/raw/master/completions/zsh/_nodebrew > _nodebrew
+fi
