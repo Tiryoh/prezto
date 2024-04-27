2ff70feb0ef50444240580e91ddc3f04db69125c
Daisuke Sato <tiryoh@gmail.com>
2024-04-27 16:18:17 +0900

    chore: Update completions

--- modules/completion/opt/update.sh
+++ modules/completion/opt/update.sh
@@ -3,17 +3,15 @@
 
 if [ "$1" = "gibo" ]; then
 	curl -sL https://github.com/simonwhitaker/gibo/raw/master/shell-completions/gibo-completion.zsh > _gibo
-fi
-if [ "$1" = "git" ]; then
+elif [ "$1" = "git" ]; then
 	curl -sL https://github.com/git/git/raw/master/contrib/completion/git-completion.zsh > _git
 	curl -sL https://github.com/git/git/raw/master/contrib/completion/git-completion.bash > git-completion.bash
-fi
-if [ "$1" = "nodebrew" ]; then
+elif [ "$1" = "nodebrew" ]; then
 	curl -sL https://github.com/hokaccha/nodebrew/raw/master/completions/zsh/_nodebrew > _nodebrew
-fi
-if [ "$1" = "catkin" ]; then
+elif [ "$1" = "catkin" ]; then
 	curl -sL https://github.com/catkin/catkin_tools/raw/master/completion/_catkin > _catkin
-fi
-if [ "$1" = "docker-compose" ]; then
+elif [ "$1" = "docker-compose" ]; then
 	curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/zsh/_docker-compose > ~/.zsh/completion/_docker-compose
+else
+	echo "Nothing to do..."
 fi
