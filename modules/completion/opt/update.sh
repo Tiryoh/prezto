fb4aa8f54d7299cb7cdd06d34ca494ae79ddb2d2
Daisuke Sato <tiryoh@gmail.com>
2024-05-07 01:18:23 +0900

    chore: Update completions

--- modules/completion/opt/update.sh
+++ modules/completion/opt/update.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env zsh
 
 
-if [ "$1" = "gibo" ]; then
-	curl -sL https://github.com/simonwhitaker/gibo/raw/master/shell-completions/gibo-completion.zsh > _gibo
-elif [ "$1" = "git" ]; then
+if [ "$1" = "git" ]; then
 	curl -sL https://github.com/git/git/raw/master/contrib/completion/git-completion.zsh > _git
 	curl -sL https://github.com/git/git/raw/master/contrib/completion/git-completion.bash > git-completion.bash
 elif [ "$1" = "nodebrew" ]; then
 	curl -sL https://github.com/hokaccha/nodebrew/raw/master/completions/zsh/_nodebrew > _nodebrew
 elif [ "$1" = "catkin" ]; then
-	curl -sL https://github.com/catkin/catkin_tools/raw/master/completion/_catkin > _catkin
+	curl -sL https://raw.githubusercontent.com/catkin/catkin_tools/main/completion/_catkin >| _catkin
 elif [ "$1" = "docker-compose" ]; then
 	curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/zsh/_docker-compose > ~/.zsh/completion/_docker-compose
+elif [ "$1" = "gh" ]; then
+	gh completion -s zsh >| _gh
+elif [ "$1" = "yq" ]; then
+	yq shell-completion zsh >| _yq
 else
 	echo "Nothing to do..."
 fi
+
+echo File checking...
+grep "</html" -rl . --exclude=update.sh && { echo error found!; exit 1; } || echo OK.
